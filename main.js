b42fe49488a28a8470fe570f9c2b372395cfe2a5
linkFly <linkFly6@live.com>
2016-04-13 19:20:47 +0800

    正在筹备基础项目模型框架

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 ﻿var app = require('app');  // 控制应用生命周期的模块。
 var BrowserWindow = require('browser-window');  // 创建原生浏览器窗口的模块
+const ipc = require('ipc');//进程通讯模块
+const compressor = require('yuicompressor');//yui压缩引擎
+const _ = require('lodash');
 
 // 保持一个对于 window 对象的全局引用，不然，当 JavaScript 被 GC，
 // window 会被自动地关闭
@@ -21,6 +24,8 @@ app.on('ready', function () {
     mainWindow = new BrowserWindow({
         //frame: false/*创建一个 Frameless Window，自己重写title最大化最小化关闭按钮*/
         width: 1000, height: 700,
+        minWidth: 960,
+        minHeight: 700,
         //autoHideMenuBar: true,
         webPreferences: {
 
@@ -32,7 +37,7 @@ app.on('ready', function () {
     mainWindow.isMenuBarVisible(false);
     mainWindow.setMenu(null);//置空菜单栏
     // 打开开发工具
-    //mainWindow.openDevTools();
+    mainWindow.openDevTools();
 
     // 当 window 被关闭，这个事件会被发出
     mainWindow.on('closed', function () {
@@ -41,4 +46,22 @@ app.on('ready', function () {
         // 但这次不是。
         mainWindow = null;
     });
+
+    //监听压缩机制
+    ipc.on('async-compressor', function (event, arg) {
+        if (typeof arg === 'string') {
+            compressor.compress(arg, {
+                charset: 'utf8',//指定读取输入文件使用的编码
+                type: 'js',//指定输入文件的文件类型 
+                //nomunge: true,//只压缩不混淆
+                //'preserve-semi': true,//保留所有的分号
+                //'line-break': 80,//在指定的列后插入一个 line-bread 符号
+                //'disable-optimizations': true//禁止优化
+            }, function (err, data, extra) {//错误，压缩后的代码，警告
+                event.sender.send('async-compressor-reply', data);
+            });
+        } else
+            event.sender.send('async-compressor-reply', null);
+    });
+
 });
