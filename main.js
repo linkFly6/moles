03b617e22d6b236a85afd96a9f231148e39f6fed
linkFly <linkFly6@live.com>
2016-04-18 18:34:14 +0800

    编写简单压缩页面

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 ﻿var app = require('app');  // 控制应用生命周期的模块。
 var BrowserWindow = require('browser-window');  // 创建原生浏览器窗口的模块
 const ipcMain = require('electron').ipcMain;//进程通讯模块
-const compressor = require('yuicompressor');//yui压缩引擎
 const _ = require('lodash');
 const shell = require('shell');
 
@@ -53,21 +52,7 @@ app.on('ready', function () {
         mainWindow = null;
     });
 
-    //监听压缩机制
-    ipcMain.on('async-compressor', function (event, arg) {
-        if (typeof arg === 'string') {
-            compressor.compress(arg, {
-                charset: 'utf8',//指定读取输入文件使用的编码
-                type: 'js',//指定输入文件的文件类型 
-                //nomunge: true,//只压缩不混淆
-                //'preserve-semi': true,//保留所有的分号
-                //'line-break': 80,//在指定的列后插入一个 line-bread 符号
-                //'disable-optimizations': true//禁止优化
-            }, function (err, data, extra) {//错误，压缩后的代码，警告
-                event.sender.send('async-compressor-reply', data);
-            });
-        } else
-            event.sender.send('async-compressor-reply', null);
-    });
+    require('./src/services/quickcompress');
+
 
 });
--- src/views/quickcompress.js
+++ src/views/quickcompress.js
@@ -2,6 +2,14 @@
     'use strict';
     app.controller('simple-Compressor-ctr', ['$scope', '$timeout', function ($scope, $timeout) {
         $scope.oldCodeSource = $scope.newCodeSource = '';
+        $scope.compressType = 0;
+        $scope.nomunge = false;///只压缩，不混淆
+        $scope.ps = false;//保留所有的分号
+        $scope.do = false;//禁止优化
+        $scope.selectType = (compressType) => {
+
+        }
+
         $scope.compressor = function () {
             var oldValue = $scope.oldCodeSource.trim();
             if (oldValue.length) {
