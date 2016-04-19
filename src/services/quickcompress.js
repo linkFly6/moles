c5485412b35f6228b9b3cb961a8ab7c19547ac36
linkFly <linkFly6@live.com>
2016-04-19 18:33:07 +0800

    首页加入程序员老黄历

--- src/services/quickcompress.js
+++ src/services/quickcompress.js
@@ -3,18 +3,26 @@
     const compressor = require('yuicompressor');//yui压缩引擎
     const _ = require('lodash');
     const ipcMain = require('electron').ipcMain;//进程通讯模块
+    const UglifyJS = require("uglify-js");//UglifyJS压缩引擎：https://github.com/mishoo/UglifyJS2#api-reference
+
+    var result = UglifyJS.minify("var b = function () {};", { fromString: true });
+
+
     //监听压缩机制
     ipcMain.on('async-compressor', function (event, arg) {
-        if (typeof arg === 'string') {
-            compressor.compress(arg, {
+        if (typeof arg === 'object') {
+            //原始字节数
+            var oldBytes = Buffer.byteLength(arg.source, 'utf8'), newBytes, startDate = +new Date;
+            compressor.compress(arg.source, {
                 charset: 'utf8',//指定读取输入文件使用的编码
-                type: 'js',//指定输入文件的文件类型 
-                //nomunge: true,//只压缩不混淆
-                //'preserve-semi': true,//保留所有的分号
-                //'line-break': 80,//在指定的列后插入一个 line-bread 符号
-                //'disable-optimizations': true//禁止优化
+                type: arg.compressType,//指定输入文件的文件类型 => js/css
+                nomunge: arg.nomunge,//只压缩不混淆,bool
+                'preserve-semi': arg.ps,//保留所有的分号,bool
+                //'line-break': 80,//在指定的列后插入一个 line-bread 符号,int
+                'disable-optimizations': arg.disableOptimizations//禁止优化,bool
             }, function (err, data, extra) {//错误，压缩后的代码，警告
-                event.sender.send('async-compressor-reply', err, data, extra);
+                if (!err) newBytes = Buffer.byteLength(data, 'utf8');//新字节数
+                event.sender.send('async-compressor-reply', err, data, extra, oldBytes, newBytes, new Date - startDate);
             });
         } else
             event.sender.send('async-compressor-reply', null);
--- src/views/quickcompress.js
+++ src/views/quickcompress.js
@@ -1,30 +1,45 @@
 ﻿(function () {
     'use strict';
+    //当前压缩代码信息
     app.controller('simple-Compressor-ctr', ['$scope', '$timeout', function ($scope, $timeout) {
-        $scope.oldCodeSource = $scope.newCodeSource = '';
+        $scope.oldCodeSource = $scope.newCodeSource = $scope.info = '';
         $scope.compressType = 0;
         $scope.nomunge = false;///只压缩，不混淆
         $scope.ps = false;//保留所有的分号
-        $scope.do = false;//禁止优化
+        $scope.disableOptimizations = false;//禁止优化
+        //是否正在压缩中
+        $scope.compressoring = false;
         $scope.selectType = (compressType) => {
-
-        }
+            //切换了选项，则清空
+            if ($scope.compressType != compressType) {
+                $scope.oldCodeSource = $scope.newCodeSource = $scope.info = '';
+            }
+            $scope.compressType = compressType;
+        };
 
         $scope.compressor = function () {
             var oldValue = $scope.oldCodeSource.trim();
             if (oldValue.length) {
-                ipcRenderer.send('async-compressor', oldValue);
+                $scope.compressoring = true;
+                ipcRenderer.send('async-compressor', {
+                    source: oldValue,
+                    nomunge: $scope.nomunge,
+                    ps: $scope.ps,
+                    disableOptimizations: $scope.disableOptimizations,
+                    compressType: $scope.compressType == 0 ? 'js' : 'css'
+                });
             }
         }
         //压缩完成
-        ipcRenderer.on('async-compressor-reply', function (event, err, value, extra) {//错误，压缩后的代码，警告
-            console.info(err);
-            console.log(extra);
+        ipcRenderer.on('async-compressor-reply', function (event, err, value, extra, oldBytes, newBytes, timer) {//事件源，错误，压缩后的代码，警告，压缩前bytes，压缩后bytes，压缩耗时（ms）
             if (value != null) {
                 $scope.$apply(function () {
                     $scope.newCodeSource = value;
+                    $scope.compressoring = false;
+                    $scope.info = `当前体积：${newBytes}byte | 原始体积：${oldBytes}byte | 比率：${(newBytes / oldBytes * 100).toFixed(2)}% | 执行时间：${timer} (ms)`;
                 });
             }
+
         });
     }]);
 })();
